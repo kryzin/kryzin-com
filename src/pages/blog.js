1dd1e07e520f16f8b81721c77cb2172aab21938b
Karolina <162272@student.uwm.edu.pl>
2023-06-23 10:45:01 +0200

    [04] discard search bar idea, too much for me

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,25 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { graphql } from 'gatsby';
 
 import Layout from '../components/layout';
 import Metadata from "../components/metadata";
 import BlogItems from '../components/blogItems';
-import SearchBar from '../components/searchBar';
 
 const Blog = ({data}) => {
-  const { search } = window.location;
-  const query = new URLSearchParams(search).get('s')
-  const [searchQuery, setSearchQuery] = useState(query || '');
 
   return (
     <Layout>
       <Metadata
         title="Blog"
         description="For all my super smart articles"
-      />
-      <SearchBar
-        searchQuery={searchQuery}
-        setSearchQuery={setSearchQuery}
       />
       <BlogItems items={data} />
     </Layout>
